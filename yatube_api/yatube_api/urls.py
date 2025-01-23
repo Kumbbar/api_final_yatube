d7541111176ee80af5b1cc11d1db71b5b8de6b03
Kumbbar <damirnasibullin8@gmail.com>
2025-01-23 19:54:37 +0500

    project ready

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,10 +1,14 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from . import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
@@ -12,3 +16,8 @@ urlpatterns = [
     ),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
